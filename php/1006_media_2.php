6ba5e41203515c60e1c5d39ba787f6bebc7ed7e5
João Pedro Vicente <expedicao01@qualitas.priv>
2026-06-24 12:02:12 -0300

    add 1006

---
+++ php/1006_media_2.php
@@ -0,0 +1,8 @@
+<?php
+$handle = fopen('php://stdin', 'r');
+$a = floatval(trim(fgets($handle)));
+$b = floatval(trim(fgets($handle)));
+$c = floatval(trim(fgets($handle)));
+$media = (($a * 2) + ($b * 3) + ($c * 5)) / 10;
+printf("MEDIA = %.2f\n", $media);
+?>
