bf0bb156243a07be1451365f658eea11966f8c4b
João Pedro Doni <donijoaodev@gmail.com>
2026-06-25 16:36:20 -0300

    colocando conteudo da licensa e incluindo 1012

---
+++ php/1012_area.php
@@ -0,0 +1,15 @@
+<?php
+fscanf(STDIN, "%f %f %f", $A, $B, $C);
+$pi = 3.14159;
+$triangulo = ($A * $C) / 2;
+$circulo   = $pi * ($C * $C);
+$trapezio  = (($A + $B) * $C) / 2;
+$quadrado  = $B * $B;
+$retangulo = $A * $B;
+
+printf("TRIANGULO: %.3f\n", $triangulo);
+printf("CIRCULO: %.3f\n", $circulo);
+printf("TRAPEZIO: %.3f\n", $trapezio);
+printf("QUADRADO: %.3f\n", $quadrado);
+printf("RETANGULO: %.3f\n", $retangulo);
+?>
