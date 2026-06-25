1516e2309a8e226454de319cac0a69084b7f8798
João Pedro Doni <donijoaodev@gmail.com>
2026-06-25 14:26:13 -0300

    agora 1010 direto com STDIN

--- php/1010_calculo_simples.php
+++ php/1010_calculo_simples.php
@@ -1,12 +1,8 @@
 <?php
-$handle = fopen('php://stdin', 'r');
-
-$line = trim(fgets($handle));
-sscanf($line, "%d %d %f", $codigo1, $numero1, $valor_peca1);
-
-$line = trim(fgets($handle));
-sscanf($line, "%d %d %f", $codigo2, $numero2, $valor_peca2);
+fscanf(STDIN, "%d %d %f", $codigo1, $numero1, $valor_peca1);
+fscanf(STDIN, "%d %d %f", $codigo2, $numero2, $valor_peca2);
 
 $valor_a_pagar = ($numero1 * $valor_peca1) + ($numero2 * $valor_peca2);
+
 printf("VALOR A PAGAR: R$ %.2f\n", $valor_a_pagar);
 ?>
